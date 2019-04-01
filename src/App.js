61d11fe61ecd4142a75b9b6e24a8d74935f15aef
Gawah Chan <gawahchan@gmail.com>
2019-04-01 21:58:58 +0100

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import martianlogo from './The_Martian_image.jpg';
+import BlackMirrorCover from './images/Black_Mirror_cover.jpg';
+import BreakingBadCover from './images/Breaking_Bad_cover.jpg';
+import DeathNoteCover from './images/Death_Note_cover.jpg';
+import GameOfThronesCover from './images/Game_Of_Thrones_cover.jpg';
+import TheWireCover from './images/The_Wire_cover.jpg';
+import WalkingDeadCover from './images/Walking_Dead_cover.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={martianlogo} alt='The Martian Logo'/>
-        <br /><br /><br />
-        <div className = "TVShowTitles">
-          <div className = "show"> Black Mirror</div>
-          <div className = "show"> Breaking Bad</div>
-          <div className = "show"> Death Note</div>
+        <div className="TVShowTitles">
+          <div className="show">
+            <img src={BlackMirrorCover} alt='The Black Mirror Cover' />
+          </div>
+          <div className="show">
+            <img src={BreakingBadCover} alt='The Breaking Bad Cover' />
+          </div>
+          <div className="show">
+            <img src={DeathNoteCover} alt='The Death note Cover' />
+          </div>
         </div>
-        <br /><br />
-        <div className = "TVShowTitles">
-          <div className = "show"> Game of Thrones</div>
-          <div className = "show"> The Walking Dead</div>
-          <div className = "show"> The Wire</div>
+        <div className="TVShowTitles">
+          <div className="show">
+            <img src={GameOfThronesCover} alt='The Game of Thrones Cover' />
+          </div>
+          <div className="show">
+            <img src={WalkingDeadCover} alt='The Walking Dead Cover' />
+          </div>
+          <div className="show">
+            <img src={TheWireCover} alt='The Wire Cover' />
+          </div>
         </div>
       </div>
     );
