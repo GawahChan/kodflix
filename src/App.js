af8d08333132e7099f3b215e15b871a0e820a88c
Gawah Chan <gawahchan@gmail.com>
2019-04-01 20:47:47 +0100

    Add 6 TV Show Titles #3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <img src={martianlogo} alt='The Martian Logo'/>
+        <br /><br /><br />
+        <div className = "TVShowTitles">
+          <div className = "show"> Black Mirror</div>
+          <div className = "show"> Breaking Bad</div>
+          <div className = "show"> Death Note</div>
+        </div>
+        <br /><br />
+        <div className = "TVShowTitles">
+          <div className = "show"> Game of Thrones</div>
+          <div className = "show"> The Walking Dead</div>
+          <div className = "show"> The Wire</div>
+        </div>
       </div>
     );
   }
