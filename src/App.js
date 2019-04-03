fe53000e3f647e464a15d91a3dea65d0d5306667
Gawah Chan <gawahchan@gmail.com>
2019-04-03 13:16:46 +0100

    Introducing Routing/Navigation #8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Details from './Details.js';
 import Gallery from './Gallery.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Gallery />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/Details' component={Details} />
+        </div>
+      </Router>
     )
   }
 }
+
 export default App;
--- src/Stack.js
+++ src/Stack.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Stack(props) {
     return (
-    <div className="show">
+    <Link to="./Details" className="show">
         <img src={props.logo} alt={`${props.name} logo`} />
         <div className="Overlay">
             <h1>{props.name}</h1>
         </div>
-    </div>
+    </Link>
     );
 }
