02575430b630193f3ecfcb9a853e84e5d90fc7f7
Gawah Chan <gawahchan@gmail.com>
2019-04-03 11:41:33 +0100

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
-import Stack from './Stack.js';
-import BlackMirrorCover from './images/Black_Mirror_cover.jpg';
-import BreakingBadCover from './images/Breaking_Bad_cover.jpg';
-import DeathNoteCover from './images/Death_Note_cover.jpg';
-import GameOfThronesCover from './images/Game_Of_Thrones_cover.jpg';
-import TheWireCover from './images/The_Wire_cover.jpg';
-import WalkingDeadCover from './images/Walking_Dead_cover.jpg';
+import Gallery from './Gallery.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="TVShowTitles">
-          <Stack name="Black Mirror" logo={BlackMirrorCover}/>
-          <Stack name="Breaking Bad" logo={BreakingBadCover}/>
-          <Stack name="Death Note" logo={DeathNoteCover}/>
-        </div>
-        <div className="TVShowTitles">
-          <Stack name="Game Of Thrones" logo={GameOfThronesCover}/>
-          <Stack name="The Walking Dead" logo={WalkingDeadCover}/>
-          <Stack name="The Wire" logo={TheWireCover}/>
-        </div>
+        <Gallery />
       </div>
-    );
+    )
   }
 }
 export default App;
