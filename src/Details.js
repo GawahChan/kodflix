8dd49d3a84d1904db45e57cfc9b2632baf1dfaca
Gawah Chan <gawahchan@gmail.com>
2019-04-05 13:21:30 +0100

    Introducing component state #13

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-export default function Details() {
+export default class Details extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      welcomeMessage: 'This is a WIP :)'
+    };
+  }
+
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({
+    welcomeMessage: 'Better things are coming!'
+    })
+  }, 3000);
+}
+
+  render() {
     return (
       <div>
-        <h1>This is a WIP Details Page :)</h1>
+        <h1>{this.state.welcomeMessage}</h1>
         <Link to='/'><h3>Back to home page</h3></Link>
       </div>
     );
   }
+}
