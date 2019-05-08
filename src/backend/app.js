a95a8dcac6392e6d0b5e160bcff4d99c89894b9e
Gawah Chan <gawahchan@gmail.com>
2019-05-08 16:08:11 +0100

    #23 Enable backend in prod

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001;
 const shows = require('./shows');
-
+const path = require('path');
 
 app.get('/rest/shows', (req, res) => res.send(shows))
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
