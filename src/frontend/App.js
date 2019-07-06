a753a8920d174400bc83cd8676988bb6e3afbdc7
Gawah Chan <gawahchan@gmail.com>
2019-07-06 13:33:57 +0100

    #31 include google analytics

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { withRouter, Route, Switch} from 'react-router-dom';
 import Details from './Components/Details/Details';
 import Gallery from './Components/Gallery/Gallery';
 import NotFound from './Not-Found.js';
 import './App.css';
 
+//constructor can be used to bind event handlers to the component, not just setting initial state
+//withRouter gives the app component three props: history, location, match
+//history.listen method is called each time a user changes URL
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('are these props?', props);
+    ReactGA.initialize('UA-143339499-1');
+    this.trackpageview(this.props.location);
+    this.props.history.listen((location) => this.trackpageview(location));
+  }
+
+  trackpageview(location) {
+    ReactGA.pageview(location.pathname + location.search + location.hash);
+  }
+
   render() {
     return (
-      <Router>
         <div>
           <Switch>
             <Route exact path='/' component={Gallery} />
@@ -16,9 +31,7 @@ class App extends Component {
             <Route exact path='/:showId' component={Details} />
           </Switch>
         </div>
-      </Router>
     )
   }
 }
-
-export default App;
+export default withRouter(App);
