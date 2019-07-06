7b7169cdff11523d0a3e935b8b21440a31bf656d
Gawah Chan <gawahchan@gmail.com>
2019-07-06 13:50:19 +0100

    #31 google analytics refactored

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -10,15 +10,15 @@ import './App.css';
 //withRouter gives the app component three props: history, location, match
 //history.listen method is called each time a user changes URL
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('are these props?', props);
+  constructor({ location, history }) {
+    super();
+    console.log('props passed', location, history);
     ReactGA.initialize('UA-143339499-1');
-    this.trackpageview(this.props.location);
-    this.props.history.listen((location) => this.trackpageview(location));
+    this.trackPageView(location);
+    history.listen((location) => this.trackPageView(location));
   }
 
-  trackpageview(location) {
+  trackPageView(location) {
     ReactGA.pageview(location.pathname + location.search + location.hash);
   }
 
