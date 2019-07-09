be3d3224da66d378ebf2fa297e992e4f3685772e
Gawah Chan <gawahchan@gmail.com>
2019-07-09 18:06:16 +0100

    #34 Create vertical menu panel

--- src/frontend/Common/Menu/Menu.js
+++ src/frontend/Common/Menu/Menu.js
@@ -3,16 +3,28 @@ import MenuIcon from './Menu.svg';
 import './Menu.css';
 
 class Menu extends Component {
-    openMenu() {
-        console.log('toggled Menu');
+    constructor() {
+        super();
+        this.state = {
+            visible: false
+        };
+    }
+
+    toggleMenu() {
+        this.setState({ visible: !this.state.visible });
     }
 
     render() {
         return (
-            <div className="menu">
-                <button className="menu-button" onClick={this.openMenu}>
+            <div className={"menu" + (this.state.visible ? " is-visible" : "")}>
+                <button className="menu-button" onClick={() => this.toggleMenu()}>
                     <img src={MenuIcon} alt="Menu" />
                 </button>
+                <div className="menu-panel">
+                    <div className="menu-options">
+                        <h1>Hello Menu</h1>
+                    </div>
+                </div>
             </div>
         );
     }
