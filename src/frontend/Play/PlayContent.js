6c9f979ecd4c3d12e673d94f2ffa683232be3581
Gawah Chan <gawahchan@gmail.com>
2019-11-07 15:57:43 +0000

    Update React Player

--- src/frontend/Play/PlayContent.js
+++ src/frontend/Play/PlayContent.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 function PlayContent({ show }) {
-    return (
-        <div className='play'>
-            <iframe width='100%' height='100%' title={show.id} 
-                src={`https://www.youtube.com/embed/${show.videoId}?autoplay=1&controls=1`}
-                frameborder='0'
-                allowfullscreen
-            />
-        </div>
-    );
+  return (
+    <div className="play">
+      <ReactPlayer
+        url={`https://www.youtube.com/embed/${show.videoId}?autoplay=1&controls=1`}
+        width="100%"
+        height="100%"
+        controls
+      />
+    </div>
+  );
 }
 
 export default PlayContent;
